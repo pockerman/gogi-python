10bfb71d24f96e548c8df7fbea1559bd6bb3f849
pockerman <a.giavaras@gmail.com>
2026-06-26 10:15:53 +0100

    fix build script aftre refactoring

--- scripts/build_protos.py
+++ scripts/build_protos.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
 
-PROTO_DIR = ROOT / "vendor/protos"
+PROTO_DIR = ROOT / "vendor/protos/gogi"
 OUT_DIR = ROOT / "src"
 
 OUT_DIR.mkdir(parents=True, exist_ok=True)
